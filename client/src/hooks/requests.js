3d449857653f5b7e8c7f42c48b23115599bb8e16
SEIFU MEKONNEN TOLOSA <tms.0xvoid@gmail.com>
2022-05-23 23:55:44 +0100

    fix typo only to face CORS errors

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,7 +1,7 @@
 const API_URL = 'http://localhost:8000'
 
 async function httpGetPlanets() {
- const response =  await fetch(`/${API_URL}/planets`)
+ const response =  await fetch(`${API_URL}/planets`)
  return await response.json()
 }
 
