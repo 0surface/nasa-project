defc2af2fddfc240c504a1c409f8d5303d8c5b7a
SEIFU MEKONNEN TOLOSA <tms.0xvoid@gmail.com>
2022-05-24 10:00:39 +0100

    Add parser code, as promise resolving data stream

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,52 @@
-const planets = []
+const fs = require('fs')
+const path = require('path')
+const { parse } = require('csv-parse')
 
-module.exports = planets
+const PLANET_FILE_PATH = path.join(
+    __dirname,
+    '..',
+    '..',
+    'data',
+    'kepler_data.csv'
+)
+
+const habitablePlanets = []
+
+function isHabitablePlanet(planet) {
+    return (
+        planet['koi_disposition'] === 'CONFIRMED' &&
+        planet['koi_insol'] > 0.36 &&
+        planet['koi_insol'] < 1.11 &&
+        planet['koi_prad'] < 1.6
+    )
+}
+
+function loadPlanetsData() {
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(PLANET_FILE_PATH)
+            .pipe(
+                parse({
+                    comment: '#',
+                    columns: true,
+                })
+            )
+            .on('data', (data) => {
+                if (isHabitablePlanet(data)) {
+                    habitablePlanets.push(data)
+                }
+            })
+            .on('error', (err) => {
+                console.log(err)
+                reject(err)
+            })
+            .on('end', () => {
+                console.log(`Done:: Count ${habitablePlanets.length}`)
+                resolve()
+            })
+    })
+}
+
+module.exports = {
+    loadPlanetsData,
+    planets: habitablePlanets,
+}
