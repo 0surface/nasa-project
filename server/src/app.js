69129f5fa479992d80b38e89178bc91bb495e31c
SEIFU MEKONNEN TOLOSA <tms.0xvoid@gmail.com>
2022-05-29 22:02:31 +0100

    fix error serving app with client side routing

--- server/src/app.js
+++ server/src/app.js
@@ -19,7 +19,7 @@ app.use('/planets', planetsRouter)
 app.use('/launches', launchesRouter)
 
 app.get('/*', (req, res) => {
-    res.sendFile(__dirname, '..', 'public', 'index.html')
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
 })
 
 module.exports = app
