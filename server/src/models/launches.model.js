7fc55d91cf9e952c3b86d9b8315f2e75ce9847e0
SEIFU MEKONNEN TOLOSA <tms.0xvoid@gmail.com>
2022-05-29 21:12:48 +0100

    Connecting frontbend to api POST /launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -7,7 +7,7 @@ const launch = {
     misson: 'Kelper Exploration X',
     rocket: 'Exploration IS1',
     launchDate: new Date('December 27, 2030'),
-    destination: 'kepler-442 b',
+    target: 'kepler-442 b',
     customer: ['ZTM', 'NASA'],
     upcoming: true,
     success: true,
