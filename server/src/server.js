a431a3745e9dbac9698ae0120818b4dfe6390c52
SEIFU MEKONNEN TOLOSA <tms.0xvoid@gmail.com>
2022-06-04 10:25:07 +0100

    add mongoose, mongodb connection code

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http')
+const mongoose = require('mongoose')
 
 const app = require('./app')
 
@@ -6,9 +7,29 @@ const { loadPlanetsData } = require('./models/planets.model')
 
 const PORT = process.env.PORT || 8000
 
+const MONGO_URL =
+    'mongodb+srv://nasa-api:4mFoiwupeX5r1f5r@cluster0.jkpu3.mongodb.net/nasa?retryWrites=true&w=majority'
+
 const server = http.createServer(app)
 
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready!')
+})
+
+mongoose.connection.on('error', (err) => {
+    console.error(err)
+})
+
+async function connectToMongo() {
+    try {
+        await mongoose.connect(MONGO_URL)
+    } catch (err) {
+        console.error('MONGODB_CONNECTION_ERROR::', err)
+    }
+}
+
 async function startServer() {
+    await connectToMongo
     await loadPlanetsData()
 
     server.listen(PORT, () => {
