9f5d24551929745e3c72beef57cba7143b86087a
SEIFU MEKONNEN TOLOSA <tms.0xvoid@gmail.com>
2022-06-08 10:50:37 +0100

    clean up dummy data, dtop mongodb

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -2,22 +2,7 @@ const axios = require('axios')
 const launchesDatabase = require('./launches.mongo')
 const planets = require('./planets.mongo')
 
-const launches = new Map()
-
 const DEFAULT_FLIGHT_NUMBER = 100
-
-const launch = {
-    flightNumber: 100, // flight_number
-    mission: 'Kelper Exploration X', //name
-    rocket: 'Exploration IS1', // rocketname
-    launchDate: new Date('December 27, 2030'), //date_local
-    target: 'Kepler-442 b', //not applicable
-    customers: ['ZTM', 'NASA'], //payload.customers for each payload
-    upcoming: true, //upcoming
-    success: true, //success
-}
-
-saveLaunch(launch)
 
 const SPACEX_API_URL = process.env.SPACEX_API_URL
 
